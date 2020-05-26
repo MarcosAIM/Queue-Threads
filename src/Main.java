9889a6a2e91c36158e70c7d7e53d9c0151926713
MarcosAIM <60239279+MarcosAIM@users.noreply.github.com>
2020-05-26 18:15:50 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         final long start_time = System.currentTimeMillis();
-        final int total_size = 2;
+        final int total_size = 2; // total number of persons
         Checkpoint A = new Checkpoint(total_size,start_time);
         Checkpoint B = new Checkpoint(total_size,start_time);
         Checkpoint C = new Checkpoint(total_size,start_time){
